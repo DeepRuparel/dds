6b88dec1d97835bc7294f6160a32390705179db4
DeepRuparel <64136286+DeepRuparel@users.noreply.github.com>
2021-03-02 18:05:03 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,7 +2,22 @@ import threading
 import binascii
 from time import sleep
 from utils import base64_to_pil_image, pil_image_to_base64
+import cv2
+import numpy as np
+import tensorflow as tf
 
+
+model = tf.keras.models.load_model("vgg_model.h5")
+tags = {"C0": "safe driving",
+        "C1": "texting - right",
+        "C2": "talking on the phone - right",
+        "C3": "texting - left",
+        "C4": "talking on the phone - left",
+        "C5": "operating the radio",
+        "C6": "drinking",
+        "C7": "reaching behind",
+        "C8": "hair and makeup",
+        "C9": "talking to passenger"}
 
 class Camera(object):
     def __init__(self, makeup_artist):
@@ -22,22 +37,29 @@ class Camera(object):
         input_str = self.to_process.pop(0)
 
         # convert it to a pil image
+        
         input_img = base64_to_pil_image(input_str)
+        input_img = cv2.resize(input_img, (224, 224))
+        input_img = np.array(input_img).reshape(-1, 224, 224, 3)
+        prediction = model.predict(img)
+        predicted_class = 'C' + str(np.where(prediction[i] == np.amax(prediction[i]))[0][0])
+        print(tags[predicted_class])
+        sleep(3)
 
         ################## where the hard work is done ############
         # output_img is an PIL image
-        output_img = self.makeup_artist.apply_makeup(input_img)
+        #output_img = self.makeup_artist.apply_makeup(input_img)
 
         # output_str is a base64 string in ascii
-        output_str = pil_image_to_base64(output_img)
+        #output_str = pil_image_to_base64(output_img)
 
         # convert eh base64 string in ascii to base64 string in _bytes_
-        self.to_output.append(binascii.a2b_base64(output_str))
+        #self.to_output.append(binascii.a2b_base64(output_str))
 
     def keep_processing(self):
         while True:
             self.process_one()
-            sleep(3)
+            sleep(1)
 
     def enqueue_input(self, input):
         self.to_process.append(input)
