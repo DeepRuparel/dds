4bd9bc71948b2b41e4d468172125e3517763cdb1
DeepRuparel <64136286+DeepRuparel@users.noreply.github.com>
2021-03-02 16:59:02 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -34,15 +34,15 @@ class Camera(object):
         # convert eh base64 string in ascii to base64 string in _bytes_
         self.to_output.append(binascii.a2b_base64(output_str))
 
-    def keep_processing(self):
-        while True:
-            self.process_one()
-            sleep(1)
+    #def keep_processing(self):
+        #while True:
+            #self.process_one()
+            #sleep(1)
 
     def enqueue_input(self, input):
         self.to_process.append(input)
 
     def get_frame(self):
         while not self.to_output:
-            sleep(2)
-        return self.to_output.pop(0)
+            sleep(3)
+        #return self.to_output.pop(0)
