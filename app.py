ad1d2a8ec46f8265f387a7009590b4905c5b09d4
DeepRuparel <64136286+DeepRuparel@users.noreply.github.com>
2021-03-03 18:37:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,9 @@ from utils import base64_to_pil_image, pil_image_to_base64
 import cv2
 import numpy as np
 import tensorflow as tf
-from pygame import mixer
 
-model = tf.keras.models.load_model("vgg_model.h5")
-tags = {"C0": "safe driving",
-        "C1": "texting - right",
-        "C2": "talking on the phone - right",
-        "C3": "texting - left",
-        "C4": "talking on the phone - left",
-        "C5": "operating the radio",
-        "C6": "drinking",
-        "C7": "reaching behind",
-        "C8": "hair and makeup",
-        "C9": "talking to passenger"}
+
+
 
 
 
@@ -32,16 +22,15 @@ app.config['DEBUG'] = True
 socketio = SocketIO(app)
 camera = Camera(Makeup_artist())
 
-
-#@socketio.on('input image', namespace='/test')
-#def test_message(input):
-#    input = input.split(",")[1]
-#    camera.enqueue_input(input)
-#    image_data = input # Do your magical Image processing here!!
-#    #image_data = image_data.decode("utf-8")
-#    image_data = "data:image/jpeg;base64," + image_data
-#    print("OUTPUT " + image_data)
-#    emit('out-image-event', {'image_data': image_data}, namespace='/test')
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input = input.split(",")[1]
+    camera.enqueue_input(input)
+    image_data = input # Do your magical Image processing here!!
+    #image_data = image_data.decode("utf-8")
+    image_data = "data:image/jpeg;base64," + image_data
+    print("OUTPUT " + image_data)
+    emit('out-image-event', {'image_data': image_data}, namespace='/test')
 #    camera.enqueue_input(base64_to_pil_image(input))
 
 
@@ -69,18 +58,10 @@ def gen():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         ret, img = camera.read()
             # cv2.imshow("Test", img)
-        sleep(3)
-        img = cv2.resize(img, (224, 224))
-        img = np.array(img).reshape(-1, 224, 224, 3)
-        prediction = model.predict(img)
-        predicted_class = 'C' + str(np.where(prediction[i] == np.amax(prediction[i]))[0][0])
-        #print(tags[predicted_class])
-        output=tags[predicted_class]
+        
+        
         #return render_template('index.html', prediction_text='Driver is  $ {}'.format(output))
-        if(output!= "safe driving"):
-                mixer.init()
-                mixer.music.load('whistle-51364.mp3')
-                mixer.music.play()
+        
 
 
 @app.route('/video_feed')
