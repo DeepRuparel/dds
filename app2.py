31492888e16ac981c177feacba5cbb8c87a6415c
DeepRuparel <64136286+DeepRuparel@users.noreply.github.com>
2021-04-26 19:53:51 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -75,39 +75,14 @@ def app_object_detection():
 
             if self.type == "noop":
                 pass
-            elif self.type == "cartoon":
-                # prepare color
-                img_color = cv2.pyrDown(cv2.pyrDown(img))
-                for _ in range(6):
-                    img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-                img_color = cv2.pyrUp(cv2.pyrUp(img_color))
-
-                # prepare edges
-                img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-                img_edges = cv2.adaptiveThreshold(
-                    cv2.medianBlur(img_edges, 7),
-                    255,
-                    cv2.ADAPTIVE_THRESH_MEAN_C,
-                    cv2.THRESH_BINARY,
-                    9,
-                    2,
-                )
-                img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-
-                # combine color and edges
-                img = cv2.bitwise_and(img_color, img_edges)
+            
             elif self.type == "edges":
                 # perform edge detection
                 if(model):
                     img = cv2.flip(img,-1)
-                else:
-                    img=cv2.flip(img,0)
+                
                 #alert.warning(predicted_class)
-            elif self.type == "rotate":
-                # rotate image
-                rows, cols, _ = img.shape
-                M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-                img = cv2.warpAffine(img, M, (cols, rows))
+            
 
             return img
 
@@ -121,7 +96,7 @@ def app_object_detection():
     )
     if webrtc_ctx.video_transformer:
         webrtc_ctx.video_transformer.type = st.radio(
-            "Select transform type", ("noop", "cartoon", "edges", "rotate")
+            "Select transform type", ("noop","edges")
         )
 
 
